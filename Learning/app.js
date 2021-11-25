b7d87457c7a191e44893637857dbd36176e1f375
Kusuma16 <kusumanetcom@gmail.com>
2021-11-25 23:12:02 +0800

    Add new

---
+++ Learning/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Item = require('./models/items')
+const app = express();
+const port = 3000;
+const mongodb = 'mongodb://localhost:27017/item-database'
+
+// Middleware function
+app.use(express.urlencoded({
+    extended: true
+}))
+
+mongoose.connect(mongodb,
+    {useNewUrlParser: true},
+    {useUnifiedTopology: true})
+    .then(() => 
+    console.log('Connected')
+).
+catch(err => console.log(err))
+
+app.set('view engine', 'ejs');
+app.listen(port);
+
+app.get('/', (req,res) => {
+    res.redirect('/get-items')
+})
+
+app.get('/get-items', (req,res) => {
+    Item.find().then(result => {
+        res.render('index', {items: result})
+    }).catch(err => console.log(err))
+})
+
+app.get('/add-item', (req,res) => {
+    res.render('add-item');
+})
+
+app.post('/items',(req,res) => {
+    console.log(req.body)
+    const addItem = Item(req.body);
+    addItem.save().then(() => {
+        res.redirect('/get-items')
+    }).catch(err => console.log(err))
+
+})
+
+app.use((req,res) => {
+    res.render('error'); 
+})
